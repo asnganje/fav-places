584e6bb37e69c4fb76b93c5a0c365ed15122fccb
asnganje <abd.nganje@gmail.com>
2026-01-22 07:22:22 +0300

    Add basic navigation logic for two screens

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import AllPlaces from "./screens/AllPlaces"
+import AddPlace from "./screens/AddPlace"
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Text>Favorite places app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="AllPlaces"
+            component={AllPlaces}
+          />
+          <Stack.Screen 
+            name="AddPlace"
+            component={AddPlace}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
